5b8c2ff79d14c82c1ece96297c71642918845dc2
yesi <x@MacBook-Air.local>
2024-02-27 15:00:45 -0600

    se cambio el form js

--- form.js
+++ form.js
@@ -1,35 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var contactForm = document.getElementById("contact_form");
-  if (contactForm) {
-      contactForm.addEventListener("submit", function(event) {
-          event.preventDefault();
-          var formData = new FormData(contactForm);
+    var contactForm = document.getElementById("contact_form");
+    if (contactForm) {
+        contactForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            var formData = new FormData(contactForm);
 
-          fetch(contactForm.action, {
-              method: "POST",
-              body: formData
-          })
-          .then(function(response) {
-              return response.json();
-          })
-          .then(function(data) {
-              var successMessage = document.getElementById("mail_success");
-              var errorMessage = document.getElementById("mail_fail");
+            fetch(contactForm.action, {
+                method: "POST",
+                body: formData
+            })
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error("Error en la solicitud. Código de estado: " + response.status);
+                }
+                return response.json();
+            })
+            .then(function(data) {
+                var successMessage = document.getElementById("mail_success");
+                var errorMessage = document.getElementById("mail_fail");
 
-              if (data.success) {
-                  successMessage.style.display = "block";
-                  errorMessage.style.display = "none";
-                  contactForm.reset();
-              } else {
-                  successMessage.style.display = "none";
-                  errorMessage.style.display = "block";
-              }
-          })
-          .catch(function(error) {
-              console.error("Error:", error);
-              var errorMessage = document.getElementById("mail_fail");
-              errorMessage.style.display = "block";
-          });
-      });
-  }
+                if (data.success) {
+                    successMessage.style.display = "block";
+                    errorMessage.style.display = "none";
+                    contactForm.reset();
+                } else {
+                    successMessage.style.display = "none";
+                    errorMessage.style.display = "block";
+                }
+            })
+            .catch(function(error) {
+                console.error("Error:", error);
+                var errorMessage = document.getElementById("mail_fail");
+                errorMessage.style.display = "block";
+            });
+        });
+    }
 });
